ec5e8b9a57c950ab80acd7a5a84b88abfcef3b1f
Lee Glendenning <glen9190@mylaurier.ca>
2017-02-09 17:27:18 -0500

    Add command line support

--- Main.java
+++ Main.java
@@ -5,15 +5,30 @@ import java.io.IOException;
 
 public class Main {
     
-    static String IRIS_DATA_PATH = "C:\\Users\\The Boss\\Desktop\\School\\CP400R\\iris.data.txt";
-    //static String IRIS_DATA_PATH = "C:\\Users\\The Boss\\Desktop\\School\\CP400R\\test1.txt";
-    static int NUM_CLUSTERS = 5;
+    static int numClusters = 5;
+    static String dataSetPath = "C:\\Users\\The Boss\\Desktop\\School\\CP400R\\iris.data.txt";
     
     public static void main(String[] args) {
         
+        switch(args.length) {
+            case 0:
+            case 1:
+                System.out.println("Usage: CP400_A2 <path-to-data-set> <k-number-of-clusters>");
+                return;
+            case 2:
+            default:
+                dataSetPath = args[1];
+                try {
+                    numClusters = Integer.parseInt(args[2]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Argument 2 must be an integer");
+                    return;
+                }
+        }
+        
         KMeans km;
         try {
-            km = new KMeans(IRIS_DATA_PATH, NUM_CLUSTERS);
+            km = new KMeans(dataSetPath, numClusters);
             km.cluster();
         } catch (IOException ex) {
             System.err.println("File not found");
